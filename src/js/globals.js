18716ef93e320bddeee8774cfffd835704cbe07b
remvst <remvst@gmail.com>
2021-08-17 00:59:23 -0400

    Also don't need difficulty yet

--- src/js/globals.js
+++ src/js/globals.js
@@ -2,5 +2,4 @@ let R, // canvas context
     G, // Game instance
     w = window,
     CANVAS = nomangle(g),
-    SHADOW_COLOR = 'rgba(0, 0, 0, 0.5)',
-    DIFFICULTY_INSTRUCTION = nomangle('Press [K] to change difficulty at any time');
+    SHADOW_COLOR = 'rgba(0, 0, 0, 0.5)';
