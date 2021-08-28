9dfbe79fb8edba7b5e930792c0f8c454b0d04f23
remvst <remvst@gmail.com>
2021-08-28 00:34:28 -0400

    Remove commented out code

--- src/js/input.js
+++ src/js/input.js
@@ -21,29 +21,10 @@ class Input {
     }
 
     direction() {
-        // const axisValue = gamepadAxisValue(0);
-        // if (abs(axisValue) > 0) return axisValue;
-
-
-        // const axisValueX = gamepadAxisValue(0);
-        // const axisValueY = gamepadAxisValue(1);
-        // if (axisValueX || axisValueY) {
-        //     const angle = atan2(axisValueY, axisValueX);
-        //     MOVEMENT_TARGET_DIRECTION.x = 400 * cos(angle);
-        //     MOVEMENT_TARGET_DIRECTION.y = 400 * sin(angle);
-        //
-        //     // console.log(angle);
-        // }
-
-        // console.log(axisValueX, axisValueY);
-
         return atan2(MOVEMENT_TARGET_DIRECTION.y, MOVEMENT_TARGET_DIRECTION.x);
     }
 
     rotation() {
-        // const axisValue = gamepadAxisValue(0);
-        // if (abs(axisValue) > 0) return axisValue;
-
         if (mobileDirection()) {
             ROTATION_ACC = mobileDirection() * 0.5;
         }
