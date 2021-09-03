4d55a6b6253fa268a735226bf58043d9f06c8327
remvst <remvst@gmail.com>
2021-09-03 19:37:23 -0400

    Fixed session not ending

--- src/js/scenes/session-scene.js
+++ src/js/scenes/session-scene.js
@@ -10,19 +10,17 @@ class SessionScene extends ParkScene {
     cycle(elapsed) {
         super.cycle(elapsed);
 
-        if (this.timeLeft > 0) {
-            this.timeLeft = max(0, this.timeLeft - elapsed);
+        this.timeLeft = max(0, this.timeLeft - elapsed);
 
-            if (this.timeLeft <= 0 && this.world.hero.landed && !this.ended) {
-                this.ended = true;
-                this.world.hero.speed = 0;
-                this.world.hero.comboTracker.locked = true;
-                this.world.hero.input = new EmptyInput();
+        if (this.timeLeft <= 0 && this.world.hero.landed && !this.ended) {
+            this.ended = true;
+            this.world.hero.speed = 0;
+            this.world.hero.comboTracker.locked = true;
+            this.world.hero.input = new EmptyInput();
 
-                G.transition('#fff', 1);
+            G.transition('#fff', 1);
 
-                setTimeout(() => G.setMenu(new EndMenu(this.score)), 2000);
-            }
+            setTimeout(() => G.setMenu(new EndMenu(this.score)), 2000);
         }
     }
 }
