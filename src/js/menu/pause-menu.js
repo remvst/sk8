8e7d4dfaa8298984f8271a4d95d449aeedc7583c
remvst <remvst@gmail.com>
2021-09-04 01:16:31 -0400

    Restart button

--- src/js/menu/pause-menu.js
+++ src/js/menu/pause-menu.js
@@ -7,6 +7,10 @@ class PauseMenu extends Menu {
                     nomangle('RESUME'),
                     () => G.setMenu(null),
                 ),
+                new Button(
+                    nomangle('RESTART'),
+                    () => G.startScene(G.scene),
+                ),
                 challengesButton(520),
                 gameSpeedButton(640),
                 mainMenuButton(760),
